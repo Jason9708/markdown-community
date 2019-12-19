bc794f75e15eb60c9458ea3f9e5008188dfcc631
CAI zhijia <1107088997@qq.com>
2019-12-20 00:06:36 +0800

    society页面开发 1.0

--- src/renderer/pages/Main/Society/index.vue
+++ src/renderer/pages/Main/Society/index.vue
@@ -1,35 +1,61 @@
 <template>
     <div class='society-wrapper'>
-        <div class='society-list'>
-            <div class='list-item' v-for='(item,index) in 2' :key='index'>
-                <!-- 封面图 -->
-                <img class='coverPic' src='../../../assets/images/mock/2.jpg'></img>
-                <!-- 文章信息 标题、简介-->
-                <div class='info'>
-                    <div class='title'>
-                        盲聋瞎
-                    </div>
-                    <div class='intro'>
-                        聋子听哑巴说瞎子看到了爱情
-                    </div>
-                </div>
-                <!-- 评论、点赞 -->
-                <div class='opacity'>
-                    <i class='icon-like' style='margin-bottom:8px;'></i>
-                    <i class='icon-comment' style='margin-bottom:8px;'></i>
-                    <i class='icon-delete'></i>
-                </div>
+        <div class='nav-list'>
+            <div class='nav-item' style='margin-right:20px;' @click='navChange'>
+                <span>HOT</span>
+            </div>
+            <div class='nav-item' @click='navChange'>
+                <span>NEW</span>
+            </div>
+            <div class='isActive'></div>
+        </div> 
+        <div class='society-article'>
+            <div class='next'>
+                <i class='icon-left'></i>
+            </div>
+            <div class='next'>
+                <i class='icon-left'></i>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import anime from 'animejs'
 export default {
     name:'Society',
     data(){
         return{
-
+            isActive:1, // 1 为 HOT 2为 NEW
+        }
+    },
+    methods:{
+        // 点击Nav触发动画
+        tiggerNavAnime:function(){
+            const Anime = anime({
+                targets:'.isActive',
+                translateX:80,
+                easing: 'spring(1, 80, 10, 5)'
+            })
+            Anime.play()
+        },
+        restoreNavAnime:function(){
+            const Anime = anime({
+                targets:'.isActive',
+                translateX:0,
+                easing: 'spring(1, 80, 10, 5)'
+            })
+            Anime.play()
+        },
+        // nav 点击事件
+        navChange:function(){
+            if(this.isActive === 1){
+                this.tiggerNavAnime()
+                this.isActive = 2
+            }else{
+                this.restoreNavAnime()
+                this.isActive = 1
+            }
         }
     }
 }
@@ -73,58 +99,44 @@ export default {
     width:100%;
     height:100%;
     display:flex;
-    .society-list{
-        flex:1;
-        flex-wrap:wrap;
-        margin:30px 10px;
-        background:#fff;
-        border-radius:5px;
-        overflow-y:auto;
-        .list-item{
+    flex-direction:column;
+    background:#fff;
+    .nav-list{
+        display:flex;
+        justify-content:flex-start;
+        align—items:center;
+        height:30px;
+        padding:20px;
+        position:relative;
+        .nav-item{
             display:flex;
-            padding:15px 20px;
-            height:80px;
-            transition:all 1s ease;
+            justify-content:center;
+            width:40px;
+            padding:5px 10px;
+            font-size:14px;
+            font-weight:600;
+            color:#303952;
             cursor:pointer;
-            .coverPic{
-                height:100%;
-                border-radius:5px;
-                box-shadow:0 0 15px -5px #000;
-            }
-            .info{
-                display:flex;
-                flex-direction:column;
-                justify-content:space-between;
-                padding:8px 15px;
-                flex:1;
-                font-family:-apple-system,system-ui,BlinkMacSystemFont,Helvetica Neue,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Arial,sans-serif;
-                .title{
-                    font-size:15px;
-                    font-weight:600;
-                    color:#303952;
-                    letter-spacing:1px;
-                }
-                .intro{
-                    font-size:12px;
-                    font-weight:500;
-                    color:rgb(178, 186, 194);
-                }
-            }
-            .opacity{
-                display:flex;
-                flex-direction:column;
-                justify-content:center;
-                align-items:center;
-                width:20px;
-                padding:20px 5px;
-                border-radius:0px 5px 5px 0px;
-                background: linear-gradient(to right top, #ffbe76, #f0932b);
-                color:#fff;
-            }
         }
-        .list-item:hover{
-            background:#ecf0f1;
+        .isActive{
+            position:absolute;
+            bottom:20px;
+            left:25px;
+            width:50px;
+            height:2px;
+            background:#f0932b;
+            border-radius:10px;
         }
+    }
+    .society-article{
+        padding:5px 20px;
+        flex:1;
+        display:flex;
+        flex-direction:column;
+        margin:0px 20px 10px 20px;
+        border-radius:5px;
+        overflow-Y:auto;
+        background:gray;
     }
 }
 </style>
